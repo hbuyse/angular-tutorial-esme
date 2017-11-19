6a447f5ab3305e9be12eb7e21774f253677b6907
pamasse <pa.masse@outlook.fr>
2017-11-19 23:54:55 +0100

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PrivateComponent } from './private/private.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    LoginComponent,
+    RegisterComponent,
+    PrivateComponent
   ],
   imports: [
     BrowserModule
